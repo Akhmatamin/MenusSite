c3ba23d8c7f32326dc98a302b5f5054433dad734
Akhmatamin <ahmatamin.raimzhanov@alatoo.edu.kg>
2025-11-20 15:17:16 +0600

    front

---
+++ front/navbar.js
@@ -0,0 +1,12 @@
+// Получаем текущий путь
+const currentPage = window.location.pathname.split("/").pop();
+
+const items = document.querySelectorAll(".bottom-bar .item");
+
+items.forEach(item => {
+  const link = item.getAttribute("href");
+
+  if (link === currentPage) {
+    item.classList.add("active");
+  }
+});
